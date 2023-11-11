e7a4c45a66619141d7e4e21e457d2b3199e0108b
z1nl0x <paulo.kreft@gmail.com>
2023-11-11 16:23:39 -0300

    nothing

--- backend/api/views.py
+++ backend/api/views.py
@@ -38,7 +38,7 @@ from products.serializers import PrimaryProductSerializer
 #     if model_data:
 #         # data['id'] = model_data.id
 #         # data['title'] = model_data.title
-#         # data['content'] = model_data.content
+#         # data['content'] = model_data.content1
 #         # data['price'] = model_data.price
 
 #         data = model_to_dict(model_data, fields=['title', 'price'])
