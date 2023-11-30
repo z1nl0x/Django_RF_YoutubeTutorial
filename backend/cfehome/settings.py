85a20d3fb103d7930b25cfdf91bef9e7562c3eae
Paulo Kreft <paulo.kreft@gmail.com>
2023-11-30 12:34:16 -0300

    Project updates

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import django_heroku
-
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
